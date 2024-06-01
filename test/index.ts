9a942d5c79a03f6871f73ec05d54b61299b21b7a
Jon McClure <jon.r.mcclure@gmail.com>
2024-06-01 15:13:48 +0100

    add option to await an element on the page before taking a screenshot

--- test/index.ts
+++ test/index.ts
@@ -58,6 +58,32 @@ describe('test Paparazzo', function () {
     const imageFiles = globSync('**/*.png', { cwd: snapshotsDir });
 
     for (const relativePath of imageFiles) {
+      if (relativePath.endsWith('awaited.png')) continue;
+      const snapshotPath = path.join(snapshotsDir, relativePath);
+      const tempPath = path.join(tempDir, relativePath);
+
+      if (!fs.existsSync(tempPath)) {
+        throw new Error(`Missing comparison image for ${relativePath}`);
+      }
+
+      const numDiffPixels = await compareImages(snapshotPath, tempPath);
+      expect(numDiffPixels).to.be.lessThan(100);
+    }
+  });
+
+  it('Should wait to snap', async function () {
+    const URL = `http://127.0.0.1:${port}/await/`;
+    await paparazzo.shoot(URL, '.paparazzo', {
+      format: 'png',
+      outDir: SNAPSHOTS_DIR,
+      awaitElement: '.awaited',
+    });
+    if (GEN_SNAPSHOTS) return;
+    const snapshotsDir = path.resolve(__dirname, '_snapshots');
+    const tempDir = path.resolve(__dirname, '_temp');
+    const imageFiles = globSync('**/awaited.png', { cwd: snapshotsDir });
+
+    for (const relativePath of imageFiles) {
       const snapshotPath = path.join(snapshotsDir, relativePath);
       const tempPath = path.join(tempDir, relativePath);
 
